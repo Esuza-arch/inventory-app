498a437c23d8be31171a56ce04ab13c0290326a4
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-12-20 15:43:06 +0300

    Add create classmethod

--- lib/models/category.py
+++ lib/models/category.py
@@ -10,5 +10,13 @@ class Category:
         )
         """
         cursor.execute(sql)
-    
-        
+
+    @classmethod
+    def create(cls, name):
+        with create_conn() as conn:
+            cursor = conn.cursor()
+            sql = "INSERT INTO categories (name) VALUES (?)"
+            cursor.execute(sql, (name,))
+            conn.commit()
+            category_id = cursor.lastrowid
+        return cls.find_by_id(category_id)    
