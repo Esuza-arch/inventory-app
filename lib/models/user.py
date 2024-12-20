4ff137526bcc543b6c0099869872d8850bdf9a1b
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-12-20 15:02:50 +0300

    Add create classmethod

---
+++ lib/models/user.py
@@ -0,0 +1,24 @@
+from db.database import create_conn
+
+class User:
+    @classmethod
+    def create_table(cls, cursor):
+        sql = """ 
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL
+        )
+        """
+        cursor.execute(sql)
+
+    @classmethod
+    def create(cls, name, email):
+        with create_conn() as conn:
+            cursor = conn.cursor()
+            sql = "INSERT INTO users (name, email) VALUES (?, ?)"
+            cursor.execute(sql, (name, email))
+            conn.commit()
+            return cls.find_by_id(cursor.lastrowid)
+    
+        
